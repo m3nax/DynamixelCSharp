5de163088af48d8ff7a01ca84c19fb01ff29fccb
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2023-03-29 19:08:42 +0200

    Added IDynamixelChannel interface

---
+++ src/DynamixelCSharp/Channels/DynamixelChannelStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace DynamixelCSharp.Channels
+{
+    /// <summary>
+    /// Represents the status of a Dynamixel communication channel.
+    /// </summary>
+    public enum DynamixelChannelStatus
+    {
+        /// <summary>
+        /// Channel is closed.
+        /// </summary>
+        Closed,
+
+        /// <summary>
+        /// Channel is open.
+        /// </summary>
+        Open,
+
+        /// <summary>
+        /// Channel is faulted.
+        /// </summary>
+        Faulted,
+    }
+}
---
+++ src/DynamixelCSharp/Channels/IDynamixelChannel.cs
@@ -0,0 +1,40 @@
+﻿namespace DynamixelCSharp.Channels
+{
+    /// <summary>
+    /// Interface represents a Dynamixel communication channel.
+    /// </summary>
+    public interface IDynamixelChannel
+    {
+        /// <summary>
+        /// Gets the channel status.
+        /// </summary>
+        DynamixelChannelStatus Status { get; }
+
+        /// <summary>
+        /// Open the channel. 
+        /// If the channel is already open, this method does nothing.
+        /// If the channel is faulted or closed, this method will try to open the channel.
+        /// </summary>
+        void Open();
+
+        /// <summary>
+        /// Close the channel.
+        /// If the channel is already closed, this method does nothing.
+        /// </summary>
+        void Close();
+
+        /// <summary>
+        /// Write a command to the channel.
+        /// </summary>
+        /// <param name="command">Command to write.</param>
+        void Write(byte[] command);
+
+        /// <summary>
+        /// Read the data returned by a read command.
+        /// </summary>
+        /// <param name="command">Read command.</param>
+        /// <param name="count">Number of byte to read.</param>
+        /// <returns></returns>
+        byte[] Read(byte[] command, int count);
+    }
+}

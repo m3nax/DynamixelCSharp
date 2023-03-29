198ea18ed0803dc431eb4c2d31f9ab12bbe4dad9
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2023-03-29 21:13:44 +0200

    Added serial channel ping command example

---
+++ examples/SerialChannelReadWrite/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using DynamixelCSharp.Channels;
+
+var channel = new DynamixelSerialChannel("COM3", 1000000);
+
+channel.Open();
+
+// Protocol 1.0 ping command for device with id 1
+var pingCommand = new byte[] { 0xFF, 0xFF, 0x01, 0x02, 0x01, 0xFB };
+
+// Send command and wait for response
+// For protocol 1.0 the ping command response length is 6 bytes
+var response = channel.Send(pingCommand, 6);
+
+// Expected response is 0xFF, 0xFF, 0x01, 0x02, 0x00, 0xFC
+foreach (var packet in response)
+{
+    Console.Write(packet.ToString("X2") + " ");
+}

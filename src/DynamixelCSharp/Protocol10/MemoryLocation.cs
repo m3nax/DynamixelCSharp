58f349d780820921a4f351559d88569612d0f718
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2023-04-03 22:36:50 +0200

    Added read command

---
+++ src/DynamixelCSharp/Protocol10/MemoryLocation.cs
@@ -0,0 +1,38 @@
+﻿namespace DynamixelCSharp.Protocol10
+{
+    /// <summary>
+    /// Represents a memory location.
+    /// </summary>
+    public struct MemoryLocation
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemoryLocation"/> struct.
+        /// </summary>
+        /// <param name="address">Address of memory location.</param>
+        public MemoryLocation(byte address)
+            : this(address, 0x01)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MemoryLocation"/> struct.
+        /// </summary>
+        /// <param name="address">Address of memory location.</param>
+        /// <param name="length">Length of the memory location.</param>
+        public MemoryLocation(byte address, byte length)
+        {
+            Address = address;
+            Length = length;
+        }
+
+        /// <summary>
+        /// Address of the memory location.
+        /// </summary>
+        public byte Address { get; }
+
+        /// <summary>
+        /// Length of the memory location.
+        /// </summary>
+        public byte Length { get; }
+    }
+}
--- src/DynamixelCSharp/Protocol10/Protocol10Client.cs
+++ src/DynamixelCSharp/Protocol10/Protocol10Client.cs
@@ -10,6 +10,7 @@ namespace DynamixelCSharp.Protocol10
         private const byte Header1 = 0xFF;
         private const byte Header2 = 0xFF;
 
+
         private readonly IDynamixelChannel dynamixelChannel;
 
         /// <summary>
@@ -30,17 +31,57 @@ namespace DynamixelCSharp.Protocol10
         {
             byte instruction = 0x01;
             byte length = 0x02;
-            byte checksum = (byte)~(deviceId + length + instruction);
+            byte checksum = CalculateChecksum(deviceId, length, instruction);
             byte responseLength = 6;
 
             byte[] command = new byte[] { Header1, Header2, deviceId, length, instruction, checksum };
 
             var response = dynamixelChannel.Send(command, responseLength);
 
+            // Check for errors
             if (response[4] != 0)
             {
                 throw new Exception();
             }
+        }
+
+        /// <summary>
+        /// Read a memory location in the device.
+        /// </summary>
+        /// <param name="deviceId">Device id.</param>
+        /// <param name="location">Location of memory to read.</param>
+        public byte[] Read(byte deviceId, MemoryLocation location)
+        {
+            byte instruction = 0x02;
+            byte length = 0x04;
+            byte checksum = CalculateChecksum(deviceId, length, instruction, location.Address, location.Length);
+            byte responseLength = 7;
+
+            byte[] command = new byte[] { Header1, Header2, deviceId, length, instruction, location.Address, location.Length, checksum };
+
+            var response = dynamixelChannel.Send(command, responseLength);
+
+            // Check for errors
+            if (response[4] != 0)
+            {
+                throw new Exception();
+            }
+
+            return response[5..6];
+        }
+
+        /// <summary>
+        /// Calculate the command checksum
+        /// </summary>
+        /// <param name="deviceId"></param>
+        /// <param name="length"></param>
+        /// <param name="instruction"></param>
+        /// <returns></returns>
+        private static byte CalculateChecksum(byte deviceId, byte length, byte instruction, params byte[] parameters)
+        {
+            var checksum = ~(deviceId + length + instruction + parameters.Sum(x => x));
+
+            return (byte)checksum;
         }
     }
 }

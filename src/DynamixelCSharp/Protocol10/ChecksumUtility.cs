af3782d51904ea57e15724a7d9b2c2167a166d46
Manuel Menegazzo <65919883+m3nax@users.noreply.github.com>
2023-05-02 23:55:52 +0200

    [TEST] Added unit test  for checksum computation (#25)
    * Fixed checksum utility class name
    
    * Added unit test for static method ChecksumUtility.CalculateFrom()
    
    * Added missimg command tests

--- src/DynamixelCSharp/Protocol10/ChecksumUtility.cs
+++ src/DynamixelCSharp/Protocol10/ChecksumUtility.cs
@@ -8,8 +8,8 @@
         /// <summary>
         /// Calculate the command checksum.
         /// </summary>
-        /// <param name="payloadBytes"></param>
-        /// <returns></returns>
+        /// <param name="payloadBytes">Payload bytes user for checksum calculation.</param>
+        /// <returns>The calculated checksum.</returns>
         public static byte CalculateFrom(IEnumerable<byte> payloadBytes)
         {
             var checksum = ~(payloadBytes.Sum(x => x));
---
+++ test/DynamixelCSharp.UnitTest/Protocol10/ChecksumUtilityTests.cs
@@ -0,0 +1,44 @@
+using DynamixelCSharp.Protocol10;
+
+namespace DynamixelCSharp.UnitTest.Protocol10
+{
+    public class ChecksumUtilityTests
+    {
+        /// <summary>
+        /// Test checksum calculation.
+        /// Checksum is computed by summing all bytes in the payload and then inverting the result.
+        /// Payload is the array of bytes between the header and the checksum.
+        /// </summary>
+        /// <param name="payload"></param>
+        /// <param name="expectedChecksum"></param>
+        [Theory]
+        // Ping command (https://emanual.robotis.com/docs/en/dxl/protocol1/#ping)
+        [InlineData(new object[] { new byte[] { 0x01, 0x02, 0x01 }, 0xFB })]
+        // Read command (https://emanual.robotis.com/docs/en/dxl/protocol1/#read)
+        [InlineData(new object[] { new byte[] { 0x01, 0x04, 0x02, 0x2B, 0x01 }, 0xCC })]
+        // Write command (https://emanual.robotis.com/docs/en/dxl/protocol1/#write)
+        [InlineData(new object[] { new byte[] { 0xFE, 0x04, 0x03, 0x03, 0x01 }, 0xF6 })]
+        // Reg Write command (https://emanual.robotis.com/docs/en/dxl/protocol1/#reg-write)
+        [InlineData(new object[] { new byte[] { 0x01, 0x05, 0x04, 0x1E, 0xF4, 0x01 }, 0xE2 })]
+        // Action command (https://emanual.robotis.com/docs/en/dxl/protocol1/#action)
+        [InlineData(new object[] { new byte[] { 0xFE, 0x02, 0x05 }, 0xFA })]
+        // Factory reset command (https://emanual.robotis.com/docs/en/dxl/protocol1/#factory-reset)
+        [InlineData(new object[] { new byte[] { 0x00, 0x02, 0x06 }, 0xF7 })]
+        // Reboot command (https://emanual.robotis.com/docs/en/dxl/protocol1/#reboot)
+        [InlineData(new object[] { new byte[] { 0x01, 0x02, 0x08 }, 0xF4 })]
+        // Reboot command (https://emanual.robotis.com/docs/en/dxl/protocol1/#sync-write)
+        [InlineData(new object[] { new byte[] { 0xFE, 0x0E, 0x83, 0x1E, 0x04, 0x00, 0x10, 0x00, 0x50, 0x01, 0x01, 0x20, 0x02, 0x60, 0x03 }, 0x67 })]
+        // Reboot command (https://emanual.robotis.com/docs/en/dxl/protocol1/#bulk-read)
+        [InlineData(new object[] { new byte[] { 0xFE, 0x09, 0x92, 0x00, 0x02, 0x01, 0x1E, 0x02, 0x02, 0x24 }, 0x1D })]
+        public void CalculateFrom_ArrayOfBytes_ReturnExpectedChecksum(byte[] payload, byte expectedChecksum)
+        {
+            // Arrange
+
+            // Act
+            var calculatedChecksum = ChecksumUtility.CalculateFrom(payload);
+
+            // Assert
+            Assert.Equal(expectedChecksum, calculatedChecksum);
+        }
+    }
+}

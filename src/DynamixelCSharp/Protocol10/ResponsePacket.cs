af55a98117b5f7b6cd651441c82fc35cecc9ea10
Manuel Menegazzo <manuel.menegazzo@outlook.com>
2023-05-04 23:10:02 +0200

    Simplified extraction of response packet fields

--- src/DynamixelCSharp/Protocol10/ResponsePacket.cs
+++ src/DynamixelCSharp/Protocol10/ResponsePacket.cs
@@ -3,7 +3,62 @@
     /// <summary>
     /// Represents a response packet returned by a device.
     /// </summary>
-    public class ResponsePacket
+    public record class ResponsePacket
     {
+        private readonly byte[] responseBytes;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResponsePacket"/> struct.
+        /// </summary>
+        /// <param name="responseBytes"></param>
+        public ResponsePacket(byte[] responseBytes)
+        {
+            if (responseBytes.Length < 6)
+            {
+                throw new ArgumentOutOfRangeException(nameof(responseBytes), $"The length of the parameter  {nameof(responseBytes)} must be greater or equal to 6");
+            }
+
+            this.responseBytes = responseBytes;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ResponsePacket"/> struct.
+        /// </summary>
+        /// <param name="responseBytes"></param>
+        public ResponsePacket(IEnumerable<byte> responseBytes)
+            : this(responseBytes.ToArray())
+        {
+        }
+
+        /// <summary>
+        /// Id of the device that sent the packet.
+        /// </summary>
+        public byte DeviceId => responseBytes[2];
+
+        /// <summary>
+        /// Length of the response packet.
+        /// </summary>
+        public byte Length => responseBytes[3];
+
+        /// <summary>
+        /// Length of the response packet.
+        /// </summary>
+        public byte Error => responseBytes[4];
+
+        /// <summary>
+        /// Checksum of the response packet.
+        /// </summary>
+        public byte Checksum => responseBytes[responseBytes.Length - 1];
+
+        /// <summary>
+        /// Parameters of the response packet.
+        /// </summary>
+        public byte[] Parameters => responseBytes[5..(responseBytes.Length - 1)];
+
+        /// <summary>
+        /// Implicit conversion from byte array to responsePacket.
+        /// </summary>
+        /// <param name="bytes"></param>
+        public static implicit operator ResponsePacket(byte[] bytes) => new ResponsePacket(bytes);
     }
 }

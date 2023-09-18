907b80d94158e8d393aa54322a35563546c74716
Manuel Menegazzo <65919883+m3nax@users.noreply.github.com>
2023-09-18 16:38:23 +0200

    Update Ax18aMemoryProfile.cs

--- src/DynamixelCSharp/Protocol10/Devices/Ax18aMemoryProfile.cs
+++ src/DynamixelCSharp/Protocol10/Devices/Ax18aMemoryProfile.cs
@@ -1,7 +1,7 @@
 ﻿namespace DynamixelCSharp.Protocol10.Devices
 {
     /// <summary>
-    /// Memory profile of the AX-12A device.
+    /// AX-18A device memory profile class.
     /// Doc: https://emanual.robotis.com/docs/en/dxl/ax/ax-18a/#control-table-of-eeprom-area.
     /// </summary>
     public class Ax18aMemoryProfile : Ax12aMemoryProfile, IMemoryProfile
